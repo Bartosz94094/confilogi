65281bafe3d8136430577ba86eb100a493f8a62b
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-10-04 23:51:31 +0200

    Add footer
    Footer has been added (HTML + desktop CSS + JS). Counter still needs some
    polish.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', () => {
+
+    //switch tabs (features section)
     
     const featuresButtons = [...document.querySelectorAll(".features>.buttons-wrapper>button")];
     const tabHeader = document.querySelector(".tab h1");
@@ -50,6 +52,8 @@ window.addEventListener('load', () => {
         })
     })
 
+    //switch questions (FAQ section)
+
     const showButtons = [...document.querySelectorAll(".question>button")];
     const faqAnswers = [...document.querySelectorAll(".question>p")];
 
@@ -58,6 +62,45 @@ window.addEventListener('load', () => {
             faqAnswers[index].classList.toggle("show");
             button.classList.toggle("show");
         })
+    })
+
+    //set counter (footer)
+
+    const counter = document.querySelector(".counter");
+    let initialNumber = 35000;
+    const givenTime = 20;
+    const intervalTime = givenTime/(initialNumber/1000);
+    let formattedNumber = new Intl.NumberFormat('de-DE', { maximumSignificantDigits: 5 }).format(initialNumber);
+
+    counter.innerHTML=formattedNumber;
+
+    const countDown = setInterval(()=>{
+        if(initialNumber>0) {
+            initialNumber-=1000;
+            counter.innerHTML=new Intl.NumberFormat('de-DE', { maximumSignificantDigits: 5 }).format(initialNumber);
+        } else {
+            clearInterval(countDown);
+        }
+    }, intervalTime*1000);
+
+    //form validation
+    const input = document.querySelector("footer form input");
+    const inputWrapper = document.querySelector("footer .input-wrapper");
+    const formButton = document.querySelector(".submit");
+
+    formButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+
+        text = input.value;
+
+        if(text == "" || text.includes(" ") || !text.includes("@") || !text.includes(".") || text[text.length-1] == ".") {
+            console.log("Wrong e-mail");
+            inputWrapper.classList.add("error");
+        } else {
+            inputWrapper.classList.remove("error");
+            console.log("You signed up to our newsletter");
+        }
+        
     })
 
 });
