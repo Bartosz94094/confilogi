d4ba8ac0edde4d4fa2a41951dde4bf7c5a9b6ed1
Bartosz Czechowicz <bartosz7czechowicz@gmail.com>
2020-10-05 17:18:34 +0200

    Add movile nav and RWD
    Project works on desktops and mobile phones (vertical)

--- scripts/script.js
+++ scripts/script.js
@@ -74,7 +74,7 @@ window.addEventListener('load', () => {
     let initialNumber = 35000;
     const givenTime = 20;
     const intervalTime = givenTime/(initialNumber/1000);
-    let formattedNumber = new Intl.NumberFormat('de-DE', { maximumSignificantDigits: 5 }).format(initialNumber);
+    const formattedNumber = new Intl.NumberFormat('de-DE', { maximumSignificantDigits: 5 }).format(initialNumber);
 
     counter.innerHTML=formattedNumber;
 
@@ -139,4 +139,24 @@ window.addEventListener('load', () => {
             modalWasVisible = true;
         }
     })
+
+    //hamburger
+    const burger = document.querySelector(".upper-nav>.first-line>button");
+
+    const closeIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="15"><path fill="#FFF" fill-rule="evenodd" d="M8 5.379L13.303.075l2.122 2.122L10.12 7.5l5.304 5.303-2.122 2.122L8 9.62l-5.303 5.304-2.122-2.122L5.88 7.5.575 2.197 2.697.075 8 5.38z"/></svg>`;
+    const burgerIconSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="15"><path fill="#242A45" fill-rule="evenodd" d="M0 0h18v3H0V0zm0 6h18v3H0V6zm0 6h18v3H0v-3z"/></svg>`;
+
+    let isMobileNavVisible = false;
+
+    burger.addEventListener("click", ()=> {
+        upperNav.classList.toggle("show");
+        isMobileNavVisible = !isMobileNavVisible;
+        
+        if(isMobileNavVisible) {
+            burger.innerHTML=closeIconSVG;
+        } else {
+            burger.innerHTML=burgerIconSVG;
+        }
+    })
+
 });
